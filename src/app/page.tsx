8a411a38cf5f0eb96a17a92e21c4afc78e227887
wahhab <wahhabawaludin2004@mail.ugm.ac.id>
2025-05-01 08:38:31 +0700

    Merge dev: Hero section adn about us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,38 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<div>
-			{/* z1: efek */}
-			<div className="absolute inset-0 z-20 pointer-events-none bg-gradient-to-b from-[#6B664E]/100 via-[#F8F0CC]/40 to-[#59543E]/95" />
+			{/* Hero section */}
+			<section className="relative h-screen">
+				{/* Background Layers */}
+				<div className="absolute inset-0 z-10 bg-[#59543E]" />
+				<div className="absolute inset-0 z-30 pointer-events-none bg-gradient-to-t from-[#59543E] from-6% via-[#F8F0CC]/10 from-10% to-[#F8F0CC]/40" />
 
-			{/* z2: Gambar */}
-			<Image
-				className="z-10 h-screen w-[40rem] rounded-t-full object-cover absolute right-[15rem] top-0"
-				width={1000}
-				height={1000}
-				src="/images/hero-section.jpeg"
-				alt="hero"
-			/>
-			<section className="h-screen p-8 w-1/2 text-justify z-30 absolute flex justify-center mt-32">
-				{/* <Hero /> */}
+				{/* Image */}
+				<Image
+					className="z-20 h-screen w-[40rem] rounded-t-full object-cover absolute right-[25rem] top-0"
+					width={1000}
+					height={1000}
+					src="/images/hero-section.jpeg"
+					alt="hero"
+				/>
 
-				{/* z3: Teks */}
-				<div className="">
-					<h2 className="text-white text-[80px] font-head">Sakinah</h2>
-					<h1 className="text-white text-[100px] font-head -mt-5">Wedding</h1>
-
-					<h4 className="font-bold mt-6 text-[#1E1E1E] text-xl">Sakinah Organizer</h4>
-
-					<p className="italic mt-6 text-[#1E1E1E] text-lg">
-						Sakinah Wedding menghadirkan layanan pernikahan islami dengan
-						sentuhan elegan dan penuh makna. Kami berkomitmen menjadikan momen
-						sakral Anda indah, berkesan, dan tak terlupakan.
-					</p>
+				{/* Text Content */}
+				<div className="absolute h-screen p-8 w-1/2 text-justify z-30 flex justify-center mt-24">
+					<div>
+						<h1 className="text-white text-[120px] font-head">Sakinah</h1>
+						<h1 className="text-white text-[140px] font-head -mt-24">
+							Wedding
+						</h1>
+						<h4 className="font-bold text-[#1E1E1E] text-xl">
+							Wedding Organizer
+						</h4>
+						<p className="italic mt-4 text-white text-lg">
+							Sakinah Wedding menghadirkan layanan pernikahan islami dengan
+							sentuhan elegan dan penuh makna. Kami berkomitmen menjadikan momen
+							sakral Anda indah, berkesan, dan tak terlupakan.
+						</p>
+						<div className="bg-[#302C29] w-[150px] h-[25px] mt-6"></div>
+					</div>
 				</div>
 			</section>
 
@@ -60,6 +66,7 @@ export default function Home() {
 				</div>
 			</section>
 
+		
 		</div>
 	);
 }
